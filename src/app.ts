5ead5dea4c59652fff2e9ea65378a6f117c22928
Adriano <adrianoact@msn.com>
2024-10-26 16:24:59 -0300

    Prisma ORM integration: fundamentals

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Adriano Tobias',
+    email: 'adriano@teste.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
