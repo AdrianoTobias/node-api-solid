fd20f31346c13873d1c751b16c574441cd5e0ea3
Adriano <adrianoact@msn.com>
2024-10-29 23:41:51 -0300

    Controllers & E2E Tests: search gym controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,9 +2,12 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
 
   app.post('/gyms', create)
 }
